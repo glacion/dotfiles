cddffe04bab4122b2205a5fcdc84a0a6fb2d62b7
Ahmetcan Güvendiren <ahmtcngvndrn@gmail.com>
2021-04-03 17:17:44 +0300

    fix

--- nvim/lua/bootstrap.lua
+++ nvim/lua/bootstrap.lua
@@ -1,12 +1,12 @@
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 local packer_url = "https://github.com/wbthomason/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.cmd(join({
-    "!git clone",
-    packer_url,
-    " ",
-    install_path,
-  }))
+  vim.cmd(
+    "!git clone " ..
+    packer_url ..
+    " " ..
+    install_path
+  )
   vim.cmd("packadd packer.nvim")
 end
