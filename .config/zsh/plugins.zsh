bdc6550d725857603869caad620132a1a81b7201
Can Güvendiren <can@glacion.com>
2023-02-28 02:21:50 +0300

    migrate to zgenom

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,23 +1,30 @@
-zgen oh-my-zsh
-zgen load romkatv/powerlevel10k powerlevel10k
+config=$HOME/.config/zsh
 
-zgen loadall << EOF
-	  Aloxaf/fzf-tab
-    zsh-users/zsh-autosuggestions
-    zsh-users/zsh-completions
-    zsh-users/zsh-syntax-highlighting
-    $HOME/.config/zsh/completions
-EOF
+if [ ! -d "${ZGENOM_DIR}" ]; then
+  git clone git@github.com:jandamm/zgenom.git "${ZGENOM_DIR}"
+fi
 
-zgen oh-my-zsh plugins/sudo
-zgen oh-my-zsh plugins/fzf
+source "${ZGENOM_DIR}/zgenom.zsh"
+source "${config}/prompt.zsh"
+if ! zgenom saved; then
+  zgenom compdef
 
-zgen oh-my-zsh plugins/aws
-zgen oh-my-zsh plugins/docker
-zgen oh-my-zsh plugins/docker-compose
-zgen oh-my-zsh plugins/helm
-zgen oh-my-zsh plugins/kubectl
-zgen oh-my-zsh plugins/poetry
-zgen oh-my-zsh plugins/python
-zgen oh-my-zsh plugins/terraform
+  zgenom oh-my-zsh
 
+  zgenom load romkatv/powerlevel10k powerlevel10k
+  zgenom load Aloxaf/fzf-tab
+  zgenom load zsh-users/zsh-autosuggestions
+  zgenom load zsh-users/zsh-completions
+  zgenom load zsh-users/zsh-syntax-highlighting
+  zgenom load "${config}/completions"
+
+  zgenom oh-my-zsh plugins/sudo
+  zgenom oh-my-zsh plugins/fzf
+
+  zgenom oh-my-zsh --completion plugins/terraform
+
+  zgenom compile "${config}/prompt.zsh"
+  zgenom compile "${config}/plugins.zsh"
+
+  zgenom save
+fi
