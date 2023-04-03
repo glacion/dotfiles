044cef263d7fac584404b51188fd8d593dc3594a
Can Güvendiren <can@glacion.com>
2023-04-03 22:43:18 +0300

    disable barbar offset

--- .config/nvim/lua/plugin/barbar.lua
+++ .config/nvim/lua/plugin/barbar.lua
@@ -6,22 +6,9 @@ return {
   },
   opts = {
     animation = true,
-    icon_separator_active = "|",
-    icon_separator_inactive = "|",
   },
   config = function(_, opts)
     require("bufferline").setup(opts)
-
-    local events = require("nvim-tree.events")
-    local bufferline = require("bufferline.api")
-
-    events.subscribe("Resize", function(size)
-      bufferline.set_offset(size, "Files")
-    end)
-
-    events.subscribe("TreeClose", function()
-      bufferline.set_offset(0)
-    end)
   end,
   keys = {
     {
