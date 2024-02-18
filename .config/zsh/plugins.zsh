427ba6114645a2bb526513018b9d5dedd6e450f3
Can Güvendiren <can@glacion.com>
2024-02-19 02:04:19 +0300

    update

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -13,6 +13,7 @@ if ! zgenom saved; then
 
   zgenom load romkatv/powerlevel10k powerlevel10k
   zgenom load Aloxaf/fzf-tab
+  zgenom load jkavan/terragrunt-oh-my-zsh-plugin
   zgenom load zsh-users/zsh-autosuggestions
   zgenom load zsh-users/zsh-completions
   zgenom load zsh-users/zsh-syntax-highlighting
@@ -21,8 +22,8 @@ if ! zgenom saved; then
   zgenom oh-my-zsh plugins/sudo
   zgenom oh-my-zsh plugins/fzf
 
-  zgenom oh-my-zsh --completion plugins/terraform
-  zgenom oh-my-zsh --completion plugins/rust
+  zgenom oh-my-zsh plugins/terraform
+  zgenom oh-my-zsh plugins/rust
 
   zgenom compile "${config}/prompt.zsh"
   zgenom compile "${config}/plugins.zsh"
